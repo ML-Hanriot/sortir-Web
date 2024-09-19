5c86447f1c4f7214e2fa614b81d563d88dd22fb8
StephMPLA <stephane.mougeot@hotmail.fr>
2024-09-19 09:40:56 +0200

    Correction erreur modification profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ParticipantController extends AbstractController
 {
@@ -15,11 +20,40 @@ class ParticipantController extends AbstractController
             'controller_name' => 'ParticipantController',
         ]);
     }
+/*
     #[Route('/participant/modification', name: 'app_modifprofil')]
     public function modifprofil(): Response
     {
         return $this->render('profil/modification_profil.html.twig', [
             'controller_name' => 'ParticipantController',
+        ]);
+    }
+*/
+    #[Route('/participant/profil/edit', name: 'profile_edit')]
+    public function editProfile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        /** @var Participant $participant */
+        $participant = $this->getUser();
+
+        // Créer le formulaire pré-rempli avec les données de l'utilisateur
+        $form = $this->createForm(RegistrationFormType::class, $participant);
+
+        // Gérer la requête du formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarder les modifications
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès.');
+
+            // Rediriger après la soumission
+            return $this->redirectToRoute('app_profil');
+        }
+
+        // Afficher le formulaire pré-rempli
+        return $this->render('profil/modification_profil.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
